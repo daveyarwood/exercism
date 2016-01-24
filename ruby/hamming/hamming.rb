cc1df1661dbda400c22ad4aab24c2b950769f8f1
Dave Yarwood <dave.yarwood@gmail.com>
2016-01-24 16:12:52 -0500

    iterate on ruby hamming

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -6,9 +6,6 @@ class Hamming
       raise ArgumentError.new("strands must be the same length")
     end
 
-    a.chars.zip(b.chars).inject(0) do |sum, ab|
-      a, b = ab
-      a != b ? sum + 1 : sum
-    end
+    a.chars.zip(b.chars).count {|(x, y)| x != y}
   end
 end
