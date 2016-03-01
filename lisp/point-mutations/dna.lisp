3241581842918ac4b3921f5bf7c1e3cc398ad78f
Dave Yarwood <dave.yarwood@gmail.com>
2016-03-01 10:33:59 -0500

    iterate on CL hamming

--- lisp/point-mutations/dna.lisp
+++ lisp/point-mutations/dna.lisp
@@ -5,11 +5,6 @@
 
 (defun hamming-distance (dna1 dna2)
   "Determine number of mutations between DNA strands by computing the Hamming Distance."
-  (when (eql (length dna1) (length dna2))
-    (let ((pairs (mapcar #'list (coerce dna1 'list) (coerce dna2 'list))))
-      (reduce #'(lambda (distance pair)
-                  (if (apply #'equal pair)
-                    distance
-                    (1+ distance)))
-              pairs
-              :initial-value 0))))
+  (when (= (length dna1) (length dna2))
+    (count-if-not #'(lambda (pair) (apply #'char= pair))
+                  (map 'list #'list dna1 dna2))))
