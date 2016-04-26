636c8a4e1662805c7d17bd8b781a04ae3cb1f5b8
Dave Yarwood <dave.yarwood@gmail.com>
2016-04-26 01:04:31 -0400

    bob in c++

--- cpp/bob/bob.cpp
+++ cpp/bob/bob.cpp
@@ -1,5 +1,17 @@
 #include "bob.h"
+#include <regex>
+
+using std::regex;
 
 const char* bob::hey(const char* utterance) {
-  return "Whatever.";
+  if (regex_search(utterance, regex("[A-Z]")) &&
+      !regex_search(utterance, regex("[a-z]"))) {
+    return "Whoa, chill out!";
+  } else if (regex_match(utterance, regex(".*\\?\\s*"))) {
+    return "Sure.";
+  } else if (regex_match(utterance, regex("\\s*"))) {
+    return "Fine. Be that way!";
+  } else {
+    return "Whatever.";
+  }
 };
