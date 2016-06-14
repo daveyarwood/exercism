255d6e0289a3ea8f4409135d89817bca9f578754
Dave Yarwood <dave.yarwood@gmail.com>
2016-06-14 09:19:00 -0400

    bob in haxe

--- haxe/bob/src/Bob.hx
+++ haxe/bob/src/Bob.hx
@@ -1,5 +1,15 @@
+using StringTools;
+
 class Bob {
   public static function hey(utterance:String) {
-    return '???';
+    if (~/[A-Z]/.match(utterance) && !~/[a-z]/.match(utterance)) {
+      return 'Whoa, chill out!';
+    } else if (!~/[a-zA-Z0-9]/.match(utterance)) {
+      return 'Fine. Be that way!';
+    } else if (utterance.endsWith('?')) {
+      return 'Sure.';
+    } else {
+      return 'Whatever.';
+    }
   }
 }
