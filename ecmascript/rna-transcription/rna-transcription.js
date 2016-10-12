029683c6e298924df9dfcfac32c6d556a5015da7
Dave Yarwood <dave.yarwood@gmail.com>
2016-10-12 11:02:55 -0400

    rna transcription in es6

---
+++ ecmascript/rna-transcription/rna-transcription.js
@@ -0,0 +1,14 @@
+const DNA_TO_RNA = {
+  'G': 'C',
+  'C': 'G',
+  'T': 'A',
+  'A': 'U'
+};
+
+class Transcriptor {
+  toRna(dna_strand) {
+    return dna_strand.split('').map(x => DNA_TO_RNA[x]).join('')
+  }
+}
+
+export default Transcriptor;
