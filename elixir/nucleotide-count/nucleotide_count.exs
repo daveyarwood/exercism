8d331c2e41f973f5bda0b8ed2a1f841938e0bb96
Dave Yarwood <dave.yarwood@gmail.com>
2016-11-01 08:45:00 -0400

    nucleotide count in elixir

---
+++ elixir/nucleotide-count/nucleotide_count.exs
@@ -0,0 +1,36 @@
+defmodule NucleotideCount do
+  @nucleotides [?A, ?C, ?G, ?T]
+
+  @doc """
+  Counts individual nucleotides in a NucleotideCount strand.
+
+  ## Examples
+
+  iex> NucleotideCount.count('AATAA', ?A)
+  4
+
+  iex> NucleotideCount.count('AATAA', ?T)
+  1
+  """
+  @spec count([char], char) :: non_neg_integer
+  def count(strand, nucleotide) do
+    Enum.count strand, &(&1 == nucleotide)
+  end
+
+  @doc """
+  Returns a summary of counts by nucleotide.
+
+  ## Examples
+
+  iex> NucleotideCount.histogram('AATAA')
+  %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
+  """
+  @spec histogram([char]) :: map
+  def histogram(strand) do
+    Enum.reduce strand,
+                %{?A => 0, ?T => 0, ?C => 0, ?G => 0},
+                fn(nucleotide, result) ->
+                  Map.update(result, nucleotide, 0, &(&1 + 1))
+                end
+  end
+end
