fae4e4ca6b32e8eee4fc5433a068703b54825252
Dave Yarwood <dave.yarwood@gmail.com>
2016-11-18 08:07:46 -0500

    rename LeapYear pseudoclass => Year, more accurate

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,14 +1,14 @@
-var LeapYear = function(year) {
+var Year = function(year) {
   this.year = year;
 };
 
-LeapYear.prototype.isDivisibleBy = function(n) {
+Year.prototype.isDivisibleBy = function(n) {
   return this.year % n == 0;
 }
 
-LeapYear.prototype.isLeap = function() {
+Year.prototype.isLeap = function() {
   return this.isDivisibleBy(400) ||
          (this.isDivisibleBy(4) && !this.isDivisibleBy(100));
 };
 
-module.exports = LeapYear;
+module.exports = Year;
