3742d0a21689dab30e7b319e2444d52c2e5aaf55
Dave Yarwood <dave.yarwood@gmail.com>
2017-01-07 15:55:51 -0500

    leap year in bash

---
+++ bash/leap/leap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]] || [[ $# -gt 1 ]] || [[ ! $1 =~ ^[0-9]{4}$ ]] ; then
+  echo "Usage: leap.sh <year>"
+  exit 1
+fi
+
+YEAR=$1
+
+if [[ $(echo $YEAR % 400 | bc) -eq 0 ]] || \
+   ([[ $(echo $YEAR % 4 | bc) -eq 0 ]] && [[ ! $(echo $YEAR % 100 | bc) -eq 0 ]])
+then
+  echo This is a leap year.
+else
+  echo This is not a leap year.
+fi
+
