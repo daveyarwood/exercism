5e0a0cfd0dda3641778d2244857b3111bdc2d3c8
Dave Yarwood <dave.yarwood@gmail.com>
2017-01-22 12:47:07 -0500

    iterate on difference of squares in lua

--- lua/difference-of-squares/difference-of-squares.lua
+++ lua/difference-of-squares/difference-of-squares.lua
@@ -1,24 +1,22 @@
 diff = {};
 
-function diff.square_of_sums(n)
-  local sum = 0
+local function sum_fn(f_each, f_total)
+  return function(n)
+    local sum = 0
 
-  for x = 1, n do
-    sum = sum + x
+    for x = 1, n do
+      sum = sum + f_each(x)
+    end
+
+    return f_total(sum)
   end
-
-  return sum * sum
 end
 
-function diff.sum_of_squares(n)
-  local sum = 0
+local function identity(x) return x end
+local function squared(x)  return x^2 end
 
-  for x = 1, n do
-    sum = sum + (x * x)
-  end
-
-  return sum
-end
+diff.square_of_sums = sum_fn(identity, squared)
+diff.sum_of_squares = sum_fn(squared, identity)
 
 function diff.difference_of_squares(n)
   return diff.square_of_sums(n) - diff.sum_of_squares(n)
