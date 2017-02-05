d81e8521c13f2d71140a60545da016b32c5acf01
Dave Yarwood <dave.yarwood@gmail.com>
2017-02-05 07:56:13 -0500

    leap year in c

---
+++ c/leap/src/leap.c
@@ -0,0 +1,8 @@
+int divisible_by(int n, int year) {
+  return year % n == 0;
+}
+
+int is_leap_year(int year) {
+  return divisible_by(400, year) ||
+         (divisible_by(4, year) && !divisible_by(100, year));
+}
