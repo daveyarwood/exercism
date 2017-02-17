4d2259b2b92a1731bc94fa44a42ec3a3f65f1dd1
Dave Yarwood <dave.yarwood@gmail.com>
2017-02-17 08:20:32 -0500

    formatting

--- elisp/anagram/anagram.el
+++ elisp/anagram/anagram.el
@@ -5,7 +5,8 @@
 
 (defun anagram? (word1 word2)
   (and (not (equal word1 word2))
-       (equal (sort-string (downcase word1)) (sort-string (downcase word2)))))
+       (equal (sort-string (downcase word1))
+              (sort-string (downcase word2)))))
 
 (defun anagrams-for (word candidates)
   (remove-if-not (apply-partially #'anagram? word) candidates))
