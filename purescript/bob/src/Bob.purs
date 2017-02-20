498d30c69c168023135941a11bc75fcbc8c4fa89
Dave Yarwood <dave.yarwood@gmail.com>
2017-02-20 08:10:30 -0500

    bob in purescript

--- purescript/bob/src/Bob.purs
+++ purescript/bob/src/Bob.purs
@@ -6,24 +6,8 @@ import Data.String.Regex as Regex
 import Data.String.Regex.Flags (noFlags)
 import Data.String.Regex.Unsafe (unsafeRegex)
 
--- WIP; running into this:
-
---  No type class instance was found for
---
---    Prim.Partial
---
---
---while checking that expression \pattern ->
---                                 \str ->
---                                   (test (...)) str
---  has type String -> String -> Boolean
---while applying a function test
---  of type Regex -> String -> Boolean
---  to argument fromRight ((regex pattern) noFlags)
---in value declaration regexTest
-
 regexTest :: String -> String -> Boolean
-regexTest pattern str = Regex.test (Either.fromRight (Regex.regex pattern noFlags)) str
+regexTest pattern str = Regex.test (unsafeRegex pattern noFlags) str
 
 containsLower :: String -> Boolean
 containsLower utterance = regexTest "[a-z]" utterance
@@ -35,7 +19,7 @@ isShouting :: String -> Boolean
 isShouting utterance = containsUpper utterance && not (containsLower utterance)
 
 isAsking :: String -> Boolean
-isAsking utterance = regexTest "?$" utterance
+isAsking utterance = regexTest "\\?$" utterance
 
 notSayingAnything :: String -> Boolean
 notSayingAnything utterance = regexTest "^[\\s]*$" utterance
