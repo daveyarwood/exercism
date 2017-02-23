bd0b44cd4ebb730c330926f9d9c907cdd4cbd2c8
Dave Yarwood <dave.yarwood@gmail.com>
2017-02-23 07:49:04 -0500

    iterate on rna transcription in bash

--- bash/rna-transcription/rna_transcription.sh
+++ bash/rna-transcription/rna_transcription.sh
@@ -1,29 +1,15 @@
 #!/bin/bash
 
-dna_to_rna() {
-  NUCLEOTIDE=$1
+validate() {
+  DNA_STRAND=$1
 
-  if   [[ "$NUCLEOTIDE" == "C" ]]; then echo "G"
-  elif [[ "$NUCLEOTIDE" == "G" ]]; then echo "C"
-  elif [[ "$NUCLEOTIDE" == "T" ]]; then echo "A"
-  elif [[ "$NUCLEOTIDE" == "A" ]]; then echo "U"
-  else echo "INVALID"
-  fi
-}
-
-STRAND=$1
-OUTFILE=$(mktemp)
-
-for (( i=0; i<${#STRAND}; i++ )); do
-  DNA=${STRAND:$i:1}
-  RNA=$(dna_to_rna $DNA)
-
-  if [[ "$RNA" == "INVALID" ]]; then
+  if [[ ! "$DNA_STRAND" =~ ^[CGTA]*$ ]]; then
     echo "Invalid nucleotide detected."
     exit 1
   fi
+}
 
-  echo -n $RNA >> $OUTFILE
-done
+DNA_STRAND=$1
+validate $DNA_STRAND
+echo $DNA_STRAND | tr CGTA GCAU
 
-cat $OUTFILE
