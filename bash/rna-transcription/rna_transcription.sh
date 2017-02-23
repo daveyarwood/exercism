536706dac9f03aaf4f681341cc9a9b8ec6b1b36b
Dave Yarwood <dave.yarwood@gmail.com>
2017-02-23 07:50:32 -0500

    iterate on rna transcription in bash

--- bash/rna-transcription/rna_transcription.sh
+++ bash/rna-transcription/rna_transcription.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+DNA=CGTA
+RNA=GCAU
+
 validate() {
   DNA_STRAND=$1
 
-  if [[ ! "$DNA_STRAND" =~ ^[CGTA]*$ ]]; then
+  if [[ ! "$DNA_STRAND" =~ ^[$DNA]*$ ]]; then
     echo "Invalid nucleotide detected."
     exit 1
   fi
@@ -11,5 +14,5 @@ validate() {
 
 DNA_STRAND=$1
 validate $DNA_STRAND
-echo $DNA_STRAND | tr CGTA GCAU
+echo $DNA_STRAND | tr $DNA $RNA
 
