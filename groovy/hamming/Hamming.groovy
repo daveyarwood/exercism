1a72c4276a172b6c73766f69b7585100ef76b355
Dave Yarwood <dave.yarwood@gmail.com>
2017-03-01 08:13:23 -0500

    hamming in groovy

---
+++ groovy/hamming/Hamming.groovy
@@ -0,0 +1,11 @@
+class Hamming {
+  def compute(String a, String b) {
+    return (1..a.length()).inject(0) { difference, i ->
+      if (b.length() < i || a[i - 1] == b[i - 1]) {
+        difference
+      } else {
+        difference + 1
+      }
+    }
+  }
+}
