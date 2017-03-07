0ca23cd4db3d0811d8be88c93bbf8589ed9d1662
Dave Yarwood <dave.yarwood@gmail.com>
2017-03-07 10:38:24 -0500

    binary in nim

---
+++ nim/binary/binary.nim
@@ -0,0 +1,28 @@
+import algorithm
+import math
+import sequtils
+import strutils
+
+# I wish you could just cast a char to a string in Nim.
+proc toString(ch: char): string =
+  repeat(ch, 1)
+
+proc parseBinaryDigit(char: char): int =
+  if not {'0'..'1'}.contains(char):
+    raise newException(ValueError, "A binary digit must be 0 or 1.")
+
+  parseInt(char.toString())
+
+
+proc binary*(str: string): int =
+  var sum = 0
+  var power = -1.0
+
+  for char in toSeq(str.items).reversed():
+    var digit = parseBinaryDigit(char)
+
+    power = power + 1
+    var digitValue = (digit.float * 2.pow(power)).int
+    sum = sum + digitValue
+
+  sum
