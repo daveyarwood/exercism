aae3c1d8c0f627d327a7a78ecac77a86ca37c1cd
Dave Yarwood <dave.yarwood@gmail.com>
2017-03-11 07:09:53 -0500

    iterate on anagram in elisp

--- elisp/anagram/anagram.el
+++ elisp/anagram/anagram.el
@@ -1,12 +1,9 @@
 (require 'cl)
 
-(defun sort-string (str)
-  (cl-sort (copy-seq str) #'<))
-
 (defun anagram? (word1 word2)
   (and (not (equal word1 word2))
-       (equal (sort-string (downcase word1))
-              (sort-string (downcase word2)))))
+       (equal (cl-sort (downcase word1) #'<)
+              (cl-sort (downcase word2) #'<))))
 
 (defun anagrams-for (word candidates)
   (remove-if-not (apply-partially #'anagram? word) candidates))
