23ba6e7326395d54adf5d2e9612428347eca9e29
Dave Yarwood <dave.yarwood@gmail.com>
2017-04-01 20:59:18 -0400

    rotational cipher in elixir

---
+++ elixir/rotational-cipher/rotational_cipher.exs
@@ -0,0 +1,26 @@
+defmodule RotationalCipher do
+  @doc """
+  Given a plaintext and amount to shift by, return a rotated string.
+
+  Example:
+  iex> RotationalCipher.rotate("Attack at dawn", 13)
+  "Nggnpx ng qnja"
+  """
+  @spec rotate(text :: String.t(), shift :: integer) :: String.t()
+  def rotate(text, shift) do
+    text
+    |> to_charlist
+    |> Enum.map(fn(char) ->
+      cond do
+        ?a <= char && char <= ?z ->
+          ?a + rem(char + shift - ?a, 26)
+        ?A <= char && char <= ?Z ->
+          ?A + rem(char + shift - ?A, 26)
+        :else ->
+          char
+      end
+    end)
+    |> to_string
+  end
+end
+
