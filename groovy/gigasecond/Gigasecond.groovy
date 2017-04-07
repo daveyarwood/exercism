27e66cf09dbeb375c3079ec332f8d993de567fb8
Dave Yarwood <dave.yarwood@gmail.com>
2017-04-07 08:50:48 -0400

    iterate on gigasecond in groovy

--- groovy/gigasecond/Gigasecond.groovy
+++ groovy/gigasecond/Gigasecond.groovy
@@ -1,10 +1,7 @@
-import static java.util.Calendar.*
-
 class Gigasecond {
   Date from(Date date) {
-    Calendar calendar = Calendar.getInstance();
-    calendar.setTime(date);
-    calendar.add(SECOND, 1000000000);
-    return calendar.getTime();
+    use (groovy.time.TimeCategory) {
+      return date + 1000000000.seconds;
+    }
   }
 }
