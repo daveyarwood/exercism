37e0856c731f1b2af7f48dcb3accc752ff27be36
Dave Yarwood <dave.yarwood@gmail.com>
2017-04-09 08:11:03 -0400

    iterate on scrabble score in perl

--- perl5/scrabble-score/Word.pm
+++ perl5/scrabble-score/Word.pm
@@ -8,14 +8,14 @@ sub letter_score {
   my ($letter) = @_;
 
   my %letter_scores = (
-    a => 1, e => 1, i => 1, o => 1, u => 1, l => 1, n => 1, r => 1, s => 1,
-    t => 1,
-    d => 2, g => 2,
-    b => 3, c => 3, m => 3, p => 3,
-    f => 4, h => 4, v => 4, w => 4, y => 4,
-    k => 5,
-    j => 8, x => 8,
-    q => 10, z => 10
+    A => 1, E => 1, I => 1, O => 1, U => 1, L => 1, N => 1, R => 1, S => 1,
+    T => 1,
+    D => 2, G => 2,
+    B => 3, C => 3, M => 3, P => 3,
+    F => 4, H => 4, V => 4, W => 4, Y => 4,
+    K => 5,
+    J => 8, X => 8,
+    Q => 10, Z => 10
   );
 
   return $letter_scores{$letter};
@@ -23,22 +23,15 @@ sub letter_score {
 
 sub new {
   my ($class, $word) = @_;
-  $word =~ s/\s//;
-
-  my $self = { "word", lc $word };
+  my $self = [ grep/\w/, split //, uc $word ];
   return bless $self, $class;
 }
 
 sub score {
   my ($self, %extension) = @_;
-
-  my $score = reduce { $a + letter_score($b) }
-                     0,
-                     (split //, $self->{word});
-
+  my $score = reduce { $a + letter_score($b) } 0, @{$self};
   $score *= (2 ** $extension{double}) if $extension{double};
   $score *= (3 ** $extension{triple}) if $extension{triple};
-
   return $score;
 }
 
