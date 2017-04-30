4292ef17717527bf3dda2ecd3873c0ec74501ddf
Dave Yarwood <dave.yarwood@gmail.com>
2017-04-29 20:39:58 -0400

    iterate on word count in c++

--- cpp/word-count/word_count.h
+++ cpp/word-count/word_count.h
@@ -1,9 +1,6 @@
 #include <map>
 
-using std::map;
-using std::string;
-
 class word_count {
   public:
-    static const map<string, int> words(const char*);
+    static const std::map<std::string, int> words(const char*);
 };
