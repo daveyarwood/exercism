ad8d0753f42bc919d6daaf4124f0330f9d6a5da4
Dave Yarwood <dave.yarwood@gmail.com>
2017-05-07 07:15:43 -0400

    hamming in c

---
+++ c/hamming/src/hamming.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int compute(char* a, char* b) {
+  int difference = 0;
+
+  char x, y;
+  int i = 0;
+
+  x = *(a + i);
+  y = *(b + i);
+
+  while (!(x == '\0' || y == '\0')) {
+    if (x != y) difference++;
+
+    i++;
+    x = *(a + i);
+    y = *(b + i);
+  }
+
+  return difference;
+}
---
+++ c/hamming/src/hamming.h
@@ -0,0 +1,7 @@
+#ifndef _HAMMING_H
+#define _HAMMING_H
+
+int compute(char* a, char* b);
+
+#endif
+
