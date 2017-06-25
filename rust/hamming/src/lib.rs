55a1e9a117f850a5dad93456489dff065d731661
Dave Yarwood <dave.yarwood@gmail.com>
2017-06-25 17:57:51 -0400

    iterate on hamming in rust

--- rust/hamming/src/lib.rs
+++ rust/hamming/src/lib.rs
@@ -1,11 +1,11 @@
-pub fn hamming_distance(a: &str, b: &str) -> Result<i64, String> {
+pub fn hamming_distance(a: &str, b: &str) -> Result<i64, &'static str> {
     if a.len() != b.len() {
-        Err("Strands must be the same size".to_string())
+        Err("Strands must be the same size")
     } else {
         Ok(a.chars()
             .zip(b.chars())
             .fold(0, |distance, (x, y)| if x == y {
-                                          distance
+                                            distance
                                         } else {
                                             distance + 1
                                         }))
