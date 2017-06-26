7391c22e01d837226122e5d1b6ca6145324961a6
Dave Yarwood <dave.yarwood@gmail.com>
2017-06-26 09:32:43 -0400

    perfect numbers in racket

---
+++ racket/perfect-numbers/perfect-numbers.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+(provide perfect-numbers)
+
+(require math srfi/1)
+
+(define (drop-last coll)
+  (if (= 0 (length coll))
+    '()
+    (drop-right coll 1)))
+
+(define (perfect? n)
+  (let* ([factors (drop-last (divisors n))]
+         [sum-of-factors (apply + factors)])
+    (= n sum-of-factors)))
+
+;; Returns the list of perfect numbers from 1 up to (but not including) `limit`.
+(define (perfect-numbers limit)
+  (for/list ([n (in-range (add1 limit))]
+             #:when (and (positive? n) (perfect? n)))
+    n))
