f3ac59c1fd4580ccc747a45a6d65c1c06e8d5d05
Dave Yarwood <dave.yarwood@gmail.com>
2017-07-30 16:58:31 -0400

    iterate on pangram in haskell

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -1,11 +1,8 @@
 module Pangram (isPangram) where
 import Data.Char (toLower)
 
-alphabet :: [Char]
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-
 hasLetter :: [Char] -> Char -> Bool
 hasLetter str char = char `elem` (map toLower str)
 
 isPangram :: String -> Bool
-isPangram text = all (hasLetter text) alphabet
+isPangram text = all (hasLetter text) ['a'..'z']
