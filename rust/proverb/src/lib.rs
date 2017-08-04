bb0440d7950c4dc2553c3011b83b03ab69d511d9
Dave Yarwood <dave.yarwood@gmail.com>
2017-08-04 10:14:45 -0400

    proverb in rust

---
+++ rust/proverb/src/lib.rs
@@ -0,0 +1,22 @@
+fn build_line(a: &str, b: &str) -> String {
+    format!("For want of a {} the {} was lost.\n", a, b)
+}
+
+fn build_last_line(things: Vec<&str>) -> String {
+    if things.len() >= 3 {
+        Some("horseshoe nail")
+    } else {
+        things.first().map(|s| s.to_owned())
+    }.map_or("".to_owned(),
+             |thing| format!("And all for the want of a {}.", thing))
+}
+
+pub fn build_proverb(things: Vec<&str>) -> String {
+    let lines: String = things.windows(2)
+                              .map(|strs| build_line(strs[0], strs[1]))
+                              .collect::<String>();
+
+    let last_line: String = build_last_line(things);
+
+    format!("{}{}", lines, last_line)
+}
