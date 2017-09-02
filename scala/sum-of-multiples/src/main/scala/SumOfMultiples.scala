f952ba000fd15b84dd22bbd757311156b6cdda55
Dave Yarwood <dave.yarwood@gmail.com>
2017-09-02 10:19:42 -0400

    iterate on sum of multiples in scala

--- scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,10 +1,6 @@
 object SumOfMultiples {
-  def isMultipleOfAny(n: Int, factors: Set[Int]): Boolean = {
-    factors.exists(n % _ == 0)
-  }
-
   def sumOfMultiples(factors: Set[Int], limit: Int): Int = {
-    (1 until limit).view.filter(isMultipleOfAny(_, factors)).sum
+    factors.flatMap(n => n until limit by n).sum
   }
 }
 
