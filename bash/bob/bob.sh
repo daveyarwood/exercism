2a0aabbd2a16dfb1995dc9d7feaa5865e1602654
Dave Yarwood <dave.yarwood@gmail.com>
2017-09-22 10:31:27 -0400

    iterate on bob in bash

--- bash/bob/bob.sh
+++ bash/bob/bob.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-shouting() {
-  [[ "$1" =~ [A-Z] ]] && [[ ! "$1" =~ [a-z] ]]
-}
+shouting() { [[ "$1" =~ [A-Z] ]] && [[ ! "$1" =~ [a-z] ]]; }
+asking() { [[ "$1" =~ \?\ *$ ]]; }
+silence() { [[ -z "${1// }" ]]; }
 
-asking() {
-  [[ "$1" =~ \?\ *$ ]]
-}
+shouting "$1" && echo "Whoa, chill out!" && exit 0
+asking "$1" && echo "Sure." && exit 0
+silence "$1" && echo "Fine. Be that way!" && exit 0
 
-silence() {
-  [[ -z "${1// }" ]]
-}
-
-if shouting "$1"; then
-  echo "Whoa, chill out!"
-elif asking "$1"; then
-  echo "Sure."
-elif silence "$1"; then
-  echo "Fine. Be that way!"
-else
-  echo "Whatever."
-fi
+echo "Whatever."
