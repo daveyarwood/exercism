8ccffb9c003ab0d203738513d67ae7f726f44c5a
Dave Yarwood <dave.yarwood@gmail.com>
2017-09-24 20:44:34 -0400

    pythagorean triplet in rust

---
+++ rust/pythagorean-triplet/src/lib.rs
@@ -0,0 +1,15 @@
+fn squared(n: i64) -> i64 { n * n }
+
+pub fn find() -> Option<i64> {
+    for c in 1..1000 {
+        for b in 1..c {
+            for a in 1..b {
+                if squared(a) + squared(b) == squared(c) && a + b + c == 1000 {
+                    return Some(a * b * c)
+                }
+            }
+        }
+    }
+
+    None
+}
