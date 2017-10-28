dbc7b61108d1227b85f2b43dbafd66588effd531
Dave Yarwood <dave.yarwood@gmail.com>
2017-10-28 09:27:05 -0400

    iterate on leap year in scala

--- scala/leap/src/main/scala/Leap.scala
+++ scala/leap/src/main/scala/Leap.scala
@@ -1,9 +1,6 @@
 object Leap {
-  private def divBy(n: Int, year: Int): Boolean = {
-    year % n == 0
-  }
-
   def leapYear(year: Int): Boolean = {
-    divBy(400, year) || (divBy(4, year) && !divBy(100, year))
+    def divBy(n: Int) = year % n == 0
+    divBy(400) || (divBy(4) && !divBy(100))
   }
 }
