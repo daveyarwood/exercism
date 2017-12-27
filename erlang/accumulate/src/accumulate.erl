aa5cd246adb48fd0cf5d89718ec8328342eea5d8
Dave Yarwood <dave.yarwood@gmail.com>
2017-12-27 06:27:54 -0500

    iterate on accumulate in erlang

--- erlang/accumulate/src/accumulate.erl
+++ erlang/accumulate/src/accumulate.erl
@@ -3,7 +3,7 @@
 -export([accumulate/2, test_version/0]).
 
 accumulate(_, []) -> [];
-accumulate(Fn, [ H | T ]) ->
-  [ Fn(H) | accumulate(Fn, T) ].
+accumulate(Fn, [H | T]) ->
+  [Fn(H) | accumulate(Fn, T)].
 
 test_version() -> 1.
