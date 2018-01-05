9b703faf8170f7da8fd8467e6a1ac86c2da5594b
Dave Yarwood <dave.yarwood@gmail.com>
2018-01-04 22:38:27 -0500

    iterate on acronym in go

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -1,20 +1,18 @@
 package acronym
 
 import (
-	"bytes"
 	"regexp"
 	"strings"
 )
 
 func Abbreviate(s string) string {
-	acronym := bytes.NewBufferString("")
-
 	wordsRegex := regexp.MustCompile("[A-Za-z]+")
 	words := wordsRegex.FindAllString(s, -1)
 
+	acronym := ""
 	for _, word := range words {
-		acronym.WriteByte(word[0])
+		acronym += string(word[0])
 	}
 
-	return strings.ToUpper(acronym.String())
+	return strings.ToUpper(acronym)
 }
