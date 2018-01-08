018be047d7fe118a46d628e442a7d502368988f2
Dave Yarwood <dave.yarwood@gmail.com>
2018-01-08 06:21:51 -0500

    twofer in javascript

---
+++ javascript/two-fer/two-fer.js
@@ -0,0 +1,8 @@
+var TwoFer = function () {};
+
+TwoFer.prototype.twoFer = function twoFer (who) {
+  if (who == undefined) return twoFer('you');
+  return 'One for ' + who + ', one for me.';
+};
+
+module.exports = TwoFer;
