39b34a40642fc017b7417c2ab60c37fbb4132716
Dave Yarwood <dave.yarwood@gmail.com>
2018-01-08 06:23:00 -0500

    iterate on twofer in javascript

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -2,7 +2,7 @@ var TwoFer = function () {};
 
 TwoFer.prototype.twoFer = function twoFer (who) {
   if (who == undefined) return twoFer('you');
-  return 'One for ' + who + ', one for me.';
+  return `One for ${who}, one for me.`;
 };
 
 module.exports = TwoFer;
