b752ad0361e1e2d5a25cb9956649dcd6c9f85f54
Dave Yarwood <dave.yarwood@gmail.com>
2018-01-09 07:38:46 -0500

    bob in f#

---
+++ fsharp/bob/Bob.fs
@@ -0,0 +1,18 @@
+﻿module Bob
+
+open System
+open System.Text.RegularExpressions
+
+let shouting (input: string): bool =
+  Regex.IsMatch(input, "[A-Z]") && not (Regex.IsMatch(input, "[a-z]"))
+
+let asking (input: string): bool =
+  Regex.IsMatch(input, "\\?\\s*$")
+
+let response (input: string): string =
+  match input with
+  | s when String.IsNullOrWhiteSpace(s) -> "Fine. Be that way!"
+  | s when shouting(s) && asking(s) -> "Calm down, I know what I'm doing!"
+  | s when shouting(s) -> "Whoa, chill out!"
+  | s when asking(s) -> "Sure."
+  | s -> "Whatever."
