b87eb26d73b8b7ecd63a751e04c11cb542d2de54
Dave Yarwood <dave.yarwood@gmail.com>
2018-01-18 06:27:31 -0500

    iterate on leap year in ecmascript

--- ecmascript/leap/leap.js
+++ ecmascript/leap/leap.js
@@ -1,9 +1,6 @@
-function divBy(n, year) {
-  return year % n == 0;
-}
-
 function isLeapYear(year) {
-  return divBy(400, year) || (divBy(4, year) && !divBy(100, year));
+  let divBy = n => year % n == 0;
+  return divBy(400) || (divBy(4) && !divBy(100));
 }
 
 export default isLeapYear;
