487dafb46685f744155e8aa9ed74c4ff59bd35e8
Dave Yarwood <dave.yarwood@gmail.com>
2018-01-19 08:07:48 -0500

    iterate on acronym in c

--- c/acronym/src/acronym.c
+++ c/acronym/src/acronym.c
@@ -1,3 +1,4 @@
+#include "acronym.h"
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,21 +8,21 @@ char* abbreviate(const char* phrase) {
   if (phrase == NULL || strlen(phrase) == 0) return NULL;
 
   char* acronym = malloc(sizeof(phrase));
-  int currentIndex = -1;
-  int skipping = 0;
+  int i = -1; // index into the acronym string we're building
 
-  for (unsigned int i = 0; i < strlen(phrase); i++) {
-    char c = phrase[i];
+  // Setup for using `strtok`. We make a copy of `phrase` because `strtok`
+  // modifies it, replacing the delimiters with NULL.
+  const char delimiters[] = " -";
+  char* phraseCopy = malloc(sizeof(phrase));
+  strcpy(phraseCopy, phrase);
 
-    if (skipping && (c == ' ' || c == '-')) {
-      skipping = 0;
-    } else if (!skipping && isalpha(c)) {
-      *(acronym + ++currentIndex) = toupper(c);
-      skipping = 1;
-    }
+  char* token = strtok(phraseCopy, delimiters);
+  while (token && isalpha(token[0])) {
+    acronym[++i] = toupper(token[0]);
+    token = strtok(NULL, delimiters);
   }
 
-  *(acronym + ++currentIndex) = '\0';
+  acronym[++i] = '\0';
 
   return acronym;
 }
