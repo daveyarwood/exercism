236227b262e3d9ef935bcdec397c95df69d172e1
Dave Yarwood <dave.yarwood@gmail.com>
2018-01-20 06:45:09 -0500

    iterate on collatz in erlang

--- erlang/collatz-conjecture/src/collatz_conjecture.erl
+++ erlang/collatz-conjecture/src/collatz_conjecture.erl
@@ -2,10 +2,9 @@
 
 -export([steps/1, test_version/0]).
 
-steps(1)   -> 0;
-steps(1.0) -> 0;
+steps(1) -> 0;
 steps(N) when N =< 0 -> {error, "Only positive numbers are allowed"};
-steps(N) when (round(N) rem 2) == 0 -> 1 + steps(N / 2);
-steps(N) when (round(N) rem 2) == 1 -> 1 + steps((3 * N) + 1).
+steps(N) when (N) rem 2 == 0 -> 1 + steps(N div 2);
+steps(N) when (N) rem 2 == 1 -> 1 + steps((3 * N) + 1).
 
 test_version() -> 2.
