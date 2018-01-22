fea9f9942676f24d91055a1301bfb23b6ac27016
Dave Yarwood <dave.yarwood@gmail.com>
2018-01-22 06:56:57 -0500

    sum of multiples in f#

---
+++ fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -0,0 +1,6 @@
+﻿module SumOfMultiples
+
+let sumOfMultiples (numbers: int list) (upperBound: int): int =
+  [1 .. upperBound - 1]
+  |> List.filter (fun n -> List.exists (fun x -> n % x = 0) numbers)
+  |> List.sum
---
+++ fsharp/sum-of-multiples/SumOfMultiplesTest.fs
@@ -0,0 +1,29 @@
+module SumOfMultiplesTest
+
+open Xunit
+open FsUnit.Xunit
+open SumOfMultiples
+
+[<Fact>]
+let ``Sum to 1`` () =
+    sumOfMultiples [3; 5] 0 |> should equal 0
+
+[<Fact>]
+let ``Sum to 3`` () =
+    sumOfMultiples [3; 5] 3 |> should equal 0
+
+[<Fact>]
+let ``Sum to 4`` () =
+    sumOfMultiples [3; 5] 4 |> should equal 3
+
+[<Fact>]
+let ``Sum to 10`` () =
+    sumOfMultiples [3; 5] 10 |> should equal 23
+
+[<Fact>]
+let ``Sum to 20`` () =
+    sumOfMultiples [7; 13; 17] 20 |> should equal 51
+
+[<Fact>]
+let ``Sum to 10000`` () =
+    sumOfMultiples [43; 47] 10000 |> should equal 2203160
