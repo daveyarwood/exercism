967a7fa3d3efc7141d6a04e0ca8870a5c6d25031
Dave Yarwood <dave.yarwood@gmail.com>
2018-01-23 21:29:21 -0500

    iterate on pangram in ES6

--- ecmascript/pangram/pangram.js
+++ ecmascript/pangram/pangram.js
@@ -1,16 +1,3 @@
-// Returns the set difference set1 - set2.
-function difference(set1, set2) {
-  return new Set([...set1].filter(x => !set2.has(x)));
-}
-
-// Returns true if set1 and set2 are the same set of elements.
-function setEquals(set1, set2) {
-  return difference(set1, set2).size == 0 &&
-         difference(set2, set1).size == 0;
-}
-
-const ALPHABET = new Set("abcdefghijklmnopqrstuvwxyz");
-
 class Pangram {
   constructor(sentence) {
     this.letters = new Set(sentence.toLowerCase()
@@ -19,7 +6,7 @@ class Pangram {
   }
 
   isPangram() {
-    return setEquals(this.letters, ALPHABET);
+    return this.letters.size == 26;
   }
 }
 
