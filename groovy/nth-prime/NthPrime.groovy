6d4dd09c697e71ba3183cfb6ff2597f92758dfa6
Dave Yarwood <dave.yarwood@gmail.com>
2018-01-24 22:17:04 -0500

    iterate on nth prime in groovy

--- groovy/nth-prime/NthPrime.groovy
+++ groovy/nth-prime/NthPrime.groovy
@@ -1,25 +1,38 @@
 class PrimeGenerator implements Iterable {
-  List<Integer> primes = []
-  // 1 is not a prime number, but adding 1 to it gives us the first prime
-  // number, 2.
-  int current = 1
-  // Then we'll add 1 to that to get 3, and after that, we'll start checking
-  // only odd numbers (interval = 2) as an optimization.
-  int interval = 1
+  Iterator numbersToCheck = {
+    // 1 is not a prime number, but adding 1 to it gives us the first prime
+    // number, 2.
+    int current = 1
+    // Then we'll add 1 to that to get 3, and after that, we'll start checking
+    // only odd numbers (interval = 2) as an optimization.
+    int interval = 1
+
+    [ // Generates an infinite range of integers: 2, 3, 5, 7, 9, 11, etc.
+      hasNext: { true },
+
+      next: {
+        if (current >= 3) interval = 2
+        current += interval
+        current
+      }
+    ] as Iterator
+  }()
+
+  List<Integer> knownPrimes = []
+
+  boolean isPrime(int n) {
+    knownPrimes.every { n % it != 0 }
+  }
 
   @Override
   Iterator iterator() {
-    [
-      hasNext: { true }, // infinitely generates prime numbers
+    [ // Infinitely generates prime numbers.
+      hasNext: { true },
+
       next: {
-        if (current >= 3) interval = 2
-
-        current += interval
-        while (primes.any { current % it == 0 })
-          current += interval
-
-        primes.add(current)
-        current
+        int prime = numbersToCheck.find { isPrime(it) }
+        knownPrimes.add(prime)
+        prime
       }
     ] as Iterator
   }
