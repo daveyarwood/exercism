1ab2fb645d0b9121116d0eb4b53f8becf4e170e0
Dave Yarwood <dave.yarwood@gmail.com>
2018-01-28 06:29:00 -0500

    iterate on hamming in scheme

--- scheme/hamming/hamming.scm
+++ scheme/hamming/hamming.scm
@@ -5,10 +5,7 @@
 
 (define (hamming-distance a b)
   (if (eqv? (string-length a) (string-length b))
-    (fold (lambda (x y acc)
-            (if (eqv? x y)
-              acc
-              (+ acc 1)))
+    (fold (lambda (x y acc) (+ acc (if (eqv? x y) 0 1)))
           0
           (string->list a)
           (string->list b))
