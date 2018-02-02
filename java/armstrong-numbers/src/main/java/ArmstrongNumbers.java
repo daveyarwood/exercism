9e34beecacddfe4c4adaad50c8d603f410e2ddf3
Dave Yarwood <dave.yarwood@gmail.com>
2018-02-02 06:44:59 -0500

    iterate on armstrong numbers in java

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,12 +1,12 @@
 import java.util.Arrays;
 
 class ArmstrongNumbers {
-	boolean isArmstrongNumber(int numberToCheck) {
+  boolean isArmstrongNumber(int numberToCheck) {
     String[] characters = Integer.toString(numberToCheck).split("");
     return Arrays.stream(characters)
                  .map(s -> new Integer(s))
                  .map(i -> Math.pow(i, characters.length))
                  .mapToInt(d -> d.intValue())
                  .sum() == numberToCheck;
-	}
+  }
 }
