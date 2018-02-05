fdc680e49cca2f2fbd69d983c16243d8ddb47cf1
Dave Yarwood <dave.yarwood@gmail.com>
2018-02-04 20:05:08 -0500

    twofer in f#

---
+++ fsharp/two-fer/TwoFer.fs
@@ -0,0 +1,6 @@
+﻿module TwoFer
+
+let rec twoFer (input: string option): string =
+  match input with
+  | None            -> twoFer (Some("you"))
+  | Some(recipient) -> sprintf "One for %s, one for me." recipient
