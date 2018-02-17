93382f59550ea1f3d4ee9638faf22298613bcb28
Dave Yarwood <dave.yarwood@gmail.com>
2018-02-16 21:20:29 -0500

    iterate on ISBN verifier in clojure

--- clojure/isbn-verifier/src/isbn_verifier.clj
+++ clojure/isbn-verifier/src/isbn_verifier.clj
@@ -7,7 +7,7 @@
     (some-> (str/replace isbn "-" "")
             (->> (re-matches #"^\d{9}[\dX]$"))
             (->> (map (fn [c] (if (= \X c) 10 (Character/digit c 10))))
-                 (map (fn [n c] (* n c)) (reverse (range 1 11)))
+                 (map (fn [n c] (* n c)) (range 10 0 -1))
                  (apply +)
                  (#(mod % 11))
                  zero?))
