1a5e63fff60586436b09a8557feb90eddc4cd011
Dave Yarwood <dave.yarwood@gmail.com>
2018-02-16 21:35:26 -0500

    iterate on ISBN verifier in clojure

--- clojure/isbn-verifier/src/isbn_verifier.clj
+++ clojure/isbn-verifier/src/isbn_verifier.clj
@@ -4,13 +4,13 @@
 (defn isbn? [isbn]
   (->
     ;; Ideally my solution would just be this:
-    (some-> (str/replace isbn "-" "")
-            (->> (re-matches #"^\d{9}[\dX]$"))
-            (->> (map (fn [c] (if (= \X c) 10 (Character/digit c 10))))
-                 (map (fn [n c] (* n c)) (range 10 0 -1))
-                 (apply +)
-                 (#(mod % 11))
-                 zero?))
+    (some->> (str/replace isbn "-" "")
+             (re-matches #"^\d{9}[\dX]$")
+             (map (fn [c] (if (= \X c) 10 (Character/digit c 10))))
+             (map (fn [n c] (* n c)) (range 10 0 -1))
+             (apply +)
+             (#(mod % 11))
+             zero?)
     ;; ...but I have to do this because the tests are expecting literally
     ;; `false`, not just `nil` which is a falsy value.
     boolean))
