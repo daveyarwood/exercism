73f6ebd0c9b134ab847a5a207e9a543e1512efa0
Dave Yarwood <dave.yarwood@gmail.com>
2018-02-26 12:16:30 -0500

    iterate on scrabble score in elm

--- elm/scrabble-score/ScrabbleScore.elm
+++ elm/scrabble-score/ScrabbleScore.elm
@@ -1,43 +1,35 @@
 module ScrabbleScore exposing (scoreWord)
 
-import Dict exposing (Dict)
-
-letterScores : Dict Char Int
-letterScores =
-  Dict.fromList
-  [ ('A', 1)
-  , ('E', 1)
-  , ('I', 1)
-  , ('O', 1)
-  , ('U', 1)
-  , ('L', 1)
-  , ('N', 1)
-  , ('R', 1)
-  , ('S', 1)
-  , ('T', 1)
-  , ('D', 2)
-  , ('G', 2)
-  , ('B', 3)
-  , ('C', 3)
-  , ('M', 3)
-  , ('P', 3)
-  , ('F', 4)
-  , ('H', 4)
-  , ('V', 4)
-  , ('W', 4)
-  , ('Y', 4)
-  , ('K', 5)
-  , ('J', 8)
-  , ('X', 8)
-  , ('Q', 10)
-  , ('Z', 10)
-  ]
-
 letterScore : Char -> Int
 letterScore letter =
-  letter
-  |> (flip Dict.get) letterScores
-  |> Maybe.withDefault 0
+  case letter of
+    'A' -> 1
+    'E' -> 1
+    'I' -> 1
+    'O' -> 1
+    'U' -> 1
+    'L' -> 1
+    'N' -> 1
+    'R' -> 1
+    'S' -> 1
+    'T' -> 1
+    'D' -> 2
+    'G' -> 2
+    'B' -> 3
+    'C' -> 3
+    'M' -> 3
+    'P' -> 3
+    'F' -> 4
+    'H' -> 4
+    'V' -> 4
+    'W' -> 4
+    'Y' -> 4
+    'K' -> 5
+    'J' -> 8
+    'X' -> 8
+    'Q' -> 10
+    'Z' -> 10
+    _   -> 0
 
 scoreWord : String -> Int
 scoreWord word =
