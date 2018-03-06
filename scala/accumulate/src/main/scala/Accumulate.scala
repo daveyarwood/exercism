230ac0705ecb881ad444ae6e246c2940173c19af
Dave Yarwood <dave.yarwood@gmail.com>
2018-03-06 11:07:46 -0500

    iterate on accumulate in scala

--- scala/accumulate/src/main/scala/Accumulate.scala
+++ scala/accumulate/src/main/scala/Accumulate.scala
@@ -3,9 +3,9 @@ import scala.annotation.tailrec
 @tailrec
 class Accumulate {
   def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = {
-    if (list.isEmpty)
-      return List()
-    else
-      return f(list.head) :: accumulate(f, list.tail)
+    list match {
+      case List()  => List()
+      case x :: xs => f(x) :: accumulate(f, xs)
+    }
   }
 }
