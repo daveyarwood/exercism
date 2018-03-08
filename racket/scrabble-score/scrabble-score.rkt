41536693f9bcdc57344a9bd636d32ec847412f4f
Dave Yarwood <dave.yarwood@gmail.com>
2018-03-07 20:37:44 -0500

    scrabble score in racket

---
+++ racket/scrabble-score/scrabble-score.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+(define (letter-score character)
+  (case character
+    [(#\A #\E #\I #\O #\U #\L #\N #\R #\S #\T) 1]
+    [(#\D #\G)                                 2]
+    [(#\B #\C #\M #\P)                         3]
+    [(#\F #\H #\V #\W #\Y)                     4]
+    [(#\K)                                     5]
+    [(#\J #\X)                                 8]
+    [(#\Q #\Z)                                 10]
+    [else                                      0]))
+
+(define (score word)
+  (for/sum ([character word])
+    (letter-score (char-upcase character))))
+
+(provide score)
