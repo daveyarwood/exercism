70e967ebd545e8fac2882e55a43fb108cf7990b5
Dave Yarwood <dave.yarwood@gmail.com>
2018-03-16 13:46:43 -0400

    raindrops in kotlin

---
+++ kotlin/raindrops/src/main/kotlin/Raindrops.kt
@@ -0,0 +1,17 @@
+typealias Rule = Pair<Int, String>
+
+val RULES: List<Rule> = listOf(Pair(3, "Pling"),
+                               Pair(5, "Plang"),
+                               Pair(7, "Plong"))
+
+private fun ruleSound(n: Int, rule: Rule): String {
+  val (factor, sound) = rule
+  return if (n % factor == 0) { sound } else { "" }
+}
+
+object Raindrops {
+  fun convert(n: Int): String {
+    val sounds = RULES.fold("") { sounds, rule -> sounds + ruleSound(n, rule) }
+    return if (sounds.isNullOrEmpty()) { n.toString() } else { sounds }
+  }
+}
