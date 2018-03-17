caece8a21117038e1f9c55736445ed328c41c1bf
Dave Yarwood <dave.yarwood@gmail.com>
2018-03-17 13:35:36 -0400

    ideal isogram in php

--- php/isogram/isogram.php
+++ php/isogram/isogram.php
@@ -1,31 +1,12 @@
 <?php
 
-// Stolen from http://php.net/manual/en/function.mb-split.php because I can't be
-// bothered to write a function that a decent language would provide in its
-// standard library.
-function str_split_utf8($input) {
-  $strlen = mb_strlen($input);
-
-  while ($strlen) {
-    $array[] = mb_substr($input, 0, 1, "UTF-8");
-    $input = mb_substr($input, 1, $strlen, "UTF-8");
-    $strlen = mb_strlen($input);
-  }
-
-  return $array;
-}
+// This would be an ideal way to solve this problem, but count_chars doesn't
+// jive well with UTF-8.
 
 function isIsogram($input) {
-  $used = array();
-
-  foreach (str_split_utf8(strtolower($input)) as $character) {
-    if (in_array($character, array("-", " ")))
-      continue;
-
-    if (array_key_exists($character, $used))
+  foreach(count_chars(strtolower($input), 1) as $i => $count) {
+    if ($count > 1 && !in_array(chr($i), array("-", " ")))
       return false;
-
-    $used[$character] = true;
   }
 
   return true;
