d4fac6c9cfde00c51382bb5b6fb306ed51ec1bc9
Dave Yarwood <dave.yarwood@gmail.com>
2018-04-09 07:28:17 -0400

    simple cipher in typescript

---
+++ typescript/simple-cipher/simple-cipher.test.ts
@@ -0,0 +1,84 @@
+import SimpleCipher from './simple-cipher'
+
+describe('Random key cipher', () => {
+    const simpleCipher = new SimpleCipher()
+
+    it('has a key made of letters', () => {
+        expect(simpleCipher.key).toMatch(/^[a-z]+$/)
+    })
+
+    // Here we take advantage of the fact that plaintext of "aaa..."
+    // outputs the key. This is a critical problem with shift ciphers, some
+    // characters will always output the key verbatim.
+    it('can encode', () => {
+        expect(simpleCipher.encode('aaaaaaaaaa')).toEqual(simpleCipher.key.substr(0, 10))
+    })
+
+    it('can decode', () => {
+        expect(simpleCipher.decode(simpleCipher.key.substr(0, 10))).toEqual('aaaaaaaaaa')
+    })
+
+    it('is reversible', () => {
+        const plaintext = 'abcdefghij'
+        expect(simpleCipher.decode(simpleCipher.encode(plaintext))).toEqual(plaintext)
+    })
+})
+
+describe('Incorrect key cipher', () => {
+    it('throws an error with an all caps key', () => {
+        expect(() => {
+            new SimpleCipher('ABCDEF')
+        }).toThrowError('Bad key')
+    })
+
+    it('throws an error with a numeric key', () => {
+        expect(() => {
+            new SimpleCipher('12345')
+        }).toThrow('Bad key')
+    })
+
+    it('throws an error with an empty key', () => {
+        expect(() => {
+            new SimpleCipher('')
+        }).toThrow('Bad key')
+    })
+})
+
+describe('Substitution cipher', () => {
+    const key = 'abcdefghij'
+    const simpleCipher = new SimpleCipher(key)
+
+    it('keeps the submitted key', () => {
+        expect(simpleCipher.key).toEqual(key)
+    })
+
+    it('can encode', () => {
+        expect(simpleCipher.encode('aaaaaaaaaa')).toEqual('abcdefghij')
+    })
+
+    it('can decode', () => {
+        expect(simpleCipher.decode('abcdefghij')).toEqual('aaaaaaaaaa')
+    })
+
+    it('is reversible', () => {
+        expect(simpleCipher.decode(simpleCipher.encode('abcdefghij'))).toEqual('abcdefghij')
+    })
+
+    it(': double shift encode', () => {
+        expect(new SimpleCipher('iamapandabear').encode('iamapandabear'))
+            .toEqual('qayaeaagaciai')
+    })
+
+    it('can wrap on encode', () => {
+        expect(simpleCipher.encode('zzzzzzzzzz')).toEqual('zabcdefghi')
+    })
+
+    it('can wrap on decode', () => {
+        expect(simpleCipher.decode('zabcdefghi')).toEqual('zzzzzzzzzz')
+    })
+
+    it('can handle messages longer than the key', () => {
+        expect(new SimpleCipher('abc').encode('iamapandabear'))
+            .toEqual('iboaqcnecbfcr')
+    })
+})
---
+++ typescript/simple-cipher/simple-cipher.ts
@@ -0,0 +1,82 @@
+// Returns the range of integers from 0 to n (exclusive).
+function range(n: number) {
+  return [...Array(n).keys()];
+}
+
+// Returns a random element from an array or string.
+function randomChoice(x: any[]|string): any {
+  return x[Math.floor(Math.random() * x.length)];
+}
+
+// Returns an array of `n` results of calling a function `f`.
+function repeatedly(n: number, f: Function): any[] {
+  return range(n).map(_ => f());
+}
+
+const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+class Alphabet {
+  // Returns the index (0-25) of a (lowercase) letter.
+  static index(letter: string): number {
+    return ALPHABET.indexOf(letter);
+  }
+
+  // Returns the (lowercase) letter at index `index` in the alphabet.
+  static letter(index: number): string {
+    if (index >= 0) {
+      return ALPHABET[index];
+    } else {
+      return ALPHABET[ALPHABET.length + index];
+    }
+  }
+
+  // Returns a random (lowercase) letter.
+  static randomLetter(): string {
+    return randomChoice(ALPHABET);
+  }
+}
+
+class SimpleCipher {
+  private static randomKey(): string {
+    return repeatedly(100, () => Alphabet.randomLetter()).join('');
+  }
+
+  readonly key: string;
+
+  constructor(key=SimpleCipher.randomKey()) {
+    if (key.match(/^[a-z]+$/))
+      this.key = key;
+    else
+      throw 'Bad key';
+  }
+
+  keyLetter(index: number): string {
+    return this.key[index % this.key.length];
+  }
+
+  shift(letter: string, amount: number) {
+    let newIndex = (Alphabet.index(letter) + amount) % 26;
+    return Alphabet.letter(newIndex);
+  }
+
+  shiftString(str: string, decoding: boolean = false) {
+    let modifier = decoding ? -1 : 1;
+
+    return range(str.length)
+             .map(i => {
+               let letter = str[i];
+               let amount = Alphabet.index(this.keyLetter(i)) * modifier;
+               return this.shift(letter, amount);
+             })
+             .join('');
+  }
+
+  encode(input: string): string {
+    return this.shiftString(input);
+  }
+
+  decode(input: string): string {
+    return this.shiftString(input, true);
+  }
+}
+
+export default SimpleCipher;
