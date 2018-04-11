32601f1d6715fa10d39702f800cd2be90e4a7e8c
Dave Yarwood <dave.yarwood@gmail.com>
2018-04-11 07:39:54 -0400

    reverse string in c#

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        var builder = new StringBuilder();
+
+        for (int i = input.Length - 1; i >= 0; i--)
+        {
+            builder.Append(input[i]);
+        }
+
+        return builder.ToString();
+    }
+}
