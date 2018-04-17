7ee272c8ad4ad67b13dfdd0ee584ed23d3bf4a98
Dave Yarwood <dave.yarwood@gmail.com>
2018-04-17 14:23:23 -0400

    iterate on difference of squares in ocaml

--- ocaml/difference-of-squares/difference_of_squares.ml
+++ ocaml/difference-of-squares/difference_of_squares.ml
@@ -1,13 +1,13 @@
 let identity n = n
 let squared n = n * n
 
-let calculate_fn each_fn total_fn =
+let calculate_fn each_fn total_fn n =
   let rec map_sum = function
       0 -> 0
-    | x -> (each_fn x) + (map_sum (x - 1))
+    | x -> each_fn x + map_sum (x - 1)
   in
-  fun n -> total_fn (map_sum n)
+  total_fn (map_sum n)
 
 let square_of_sum = calculate_fn identity squared
 let sum_of_squares = calculate_fn squared identity
-let difference_of_squares n = (square_of_sum n) - (sum_of_squares n)
+let difference_of_squares n = square_of_sum n - sum_of_squares n
