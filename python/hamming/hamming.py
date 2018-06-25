3d85cdb866c5d15d5916b3b0162fb71d07362f7a
Dave Yarwood <dave.yarwood@gmail.com>
2018-06-25 09:09:42 -0400

    iterate on hamming in python

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -2,9 +2,4 @@ def distance(strand_a, strand_b):
     if (len(strand_a) != len(strand_b)):
         raise ValueError("Strands must be the same length.")
 
-    distance = 0
-
-    for (x, y) in zip(strand_a, strand_b):
-        if x != y: distance += 1
-
-    return distance
+    return len([x for x, y in zip(strand_a, strand_b) if x != y])
