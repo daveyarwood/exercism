c2118554c4dd909a559f7bd50c1dca9de975564f
Dave Yarwood <dave.yarwood@gmail.com>
2018-06-30 23:29:55 -0400

    iterate on flatten array in scala

--- scala/flatten-array/src/main/scala/FlattenArray.scala
+++ scala/flatten-array/src/main/scala/FlattenArray.scala
@@ -1,9 +1,8 @@
 object FlattenArray {
   def flatten(x: Any): List[Int] = {
-    if (x == null) return List()
-
     x match {
-      case n: Int => List(n)
+      case null            => List()
+      case n: Int          => List(n)
       case list: List[Any] => List.concat(list.map(flatten):_*)
     }
   }
